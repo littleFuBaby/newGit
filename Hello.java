4ae909e91caf80c080722bbc4f026a8993c19143
root <root@localhost.(none)>
2016-06-14 17:44:52 -0700

    fuck

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello{
 	public static void main(String[]args){
 		int i = 0;
-		System.out.println("Hello world!"+i);
+		int j = 0;
+		System.out.println("Hello world!"+i+j);
+	
+
+
+	
 	}
 }
