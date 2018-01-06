6847dbae608e4012245381fd8b2eb56af941ef1e
'littleFuBaby' <'520fys008@163.com'>
2018-01-06 16:14:06 +0800

    add own utility project

---
+++ ownutil/src/test/java/org/fuys/ownutil/util/TestDateUtil.java
@@ -0,0 +1,16 @@
+package org.fuys.ownutil.util;
+
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import junit.framework.TestCase;
+
+public class TestDateUtil {
+	
+	@Test
+	public void testGetNow() {
+		LoggerFactory.getLogger(TestDateUtil.class).info("Test org.fuys.ownutil.util.DateUtils.getNow()");
+		TestCase.assertTrue(DateUtils.getNow()!=null);
+	}
+
+}
