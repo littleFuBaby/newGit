d663752e0f2d6b704c04ac7b6704f788dbab350f
'littleFuBaby' <'520fys008@163.com'>
2018-03-16 22:01:52 +0800

    update StringUtils

--- ownutil/src/main/java/org/fuys/ownutil/util/StringUtils.java
+++ ownutil/src/main/java/org/fuys/ownutil/util/StringUtils.java
@@ -6,5 +6,23 @@ package org.fuys.ownutil.util;
  *
  */
 public class StringUtils {
+	
+	/**
+	 * If string is number , return true
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNumber(String str){
+		if(str==null ||str.length()==0){
+			return false;
+		}
+		char[] numberChars = str.toCharArray();
+		for(int x=0;x<numberChars.length;x++){
+			if(numberChars[x]<'0' || numberChars[x]>'9'){
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
