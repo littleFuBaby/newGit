5b61925897f7708304f7c08bb23f3317bc2df4e2
'littleFuBaby' <'520fys008@163.com'>
2018-03-29 22:47:43 +0800

    Runtime.getRuntime().availableProcessors()

--- ownutil/src/main/java/org/fuys/ownutil/instance/RuntimeInstance.java
+++ ownutil/src/main/java/org/fuys/ownutil/instance/RuntimeInstance.java
@@ -1,11 +1,10 @@
 package org.fuys.ownutil.instance;
 
-import java.util.Random;
-
 public class RuntimeInstance {
 	
 	public static void main(String[] args) throws CloneNotSupportedException {
 		Runtime runtime = Runtime.getRuntime();
+		System.out.println(Runtime.getRuntime().availableProcessors());
 		System.out.println(runtime.maxMemory() / (double)1024 / 1024);
 		System.out.println(runtime.totalMemory());
 		System.out.println(runtime.freeMemory());
@@ -18,11 +17,11 @@ public class RuntimeInstance {
 		System.out.println(runtime.totalMemory());
 		System.out.println(runtime.freeMemory());
 		// perm/metaspace OOM
-		String str = "www.chinal.com";
+		/*String str = "www.chinal.com";
 		while(true){
-			str = str + new Random().nextInt(99999);
+			str += str + str;
 			str.intern();
-		}
+		}*/
 	}
 
 }
