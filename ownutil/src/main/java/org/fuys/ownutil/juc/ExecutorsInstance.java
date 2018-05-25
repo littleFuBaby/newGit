2ba91ddc22c22922097dee3c06fbc0b51fe4fed9
'littleFuBaby' <'520fys008@163.com'>
2018-05-25 13:12:15 +0800

    add ThreadPoolExecutor

--- ownutil/src/main/java/org/fuys/ownutil/juc/ExecutorsInstance.java
+++ ownutil/src/main/java/org/fuys/ownutil/juc/ExecutorsInstance.java
@@ -3,43 +3,53 @@ package org.fuys.ownutil.juc;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-class Task implements Runnable {
-
-	@Override
-	public void run() {
-
-		try {
-			TimeUnit.SECONDS.sleep(new Random().nextInt(5));
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println(Thread.currentThread().getName());
-	}
-
-}
 
 public class ExecutorsInstance {
 
-	public static void main(String[] args) {
-		ExecutorService pool = Executors.newFixedThreadPool(3);
+	static class Task implements Runnable {
 
-		pool.execute(new Task());
-		pool.execute(new Task());
-		pool.submit(new Task());
-		pool.execute(new Task());
-		pool.execute(new Task());
+		private String name;
 
-		try {
-			TimeUnit.SECONDS.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		public Task(String name) {
+			this.name = name;
 		}
-		pool.shutdownNow();
-		
-		System.out.println(pool.isTerminated());
-		
+
+		@Override
+		public void run() {
+			try {
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(300));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + "  " + this.name);
+			System.out.println(poolInfo());
+		}
+	}
+
+	public static String poolInfo() {
+		ThreadPoolExecutor tpe = (ThreadPoolExecutor) pool;
+		return "corepoolsize=" + tpe.getCorePoolSize() + ",MaximumPoolSize=" + tpe.getMaximumPoolSize()
+				+ ",CompletedTaskCount=" + tpe.getCompletedTaskCount() + ",TaskCount=" + tpe.getTaskCount()
+				+ ",isShutdown=" + tpe.isShutdown() + ",isTerminated=" + tpe.isTerminated() + ",isTerminating"
+				+ tpe.isTerminating();
+	}
+
+	public static ExecutorService pool = null;
+
+	static {
+		pool = Executors.newFixedThreadPool(5);
+		ThreadPoolExecutor tpe = (ThreadPoolExecutor) pool;
+		tpe.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+	}
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		while (i++ < 1000) {
+			pool.execute(new Task("t-" + i));
+		}
 	}
 
 }
